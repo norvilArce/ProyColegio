addda2d6b52ccc68808a61c6ab5c580e9b2e8c66
Norvil Arce <norvil.arce@gmail.com>
2020-10-12 22:05:45 -0500

    listos usuario y ubigeo

---
+++ ProyColegio_ADO/UsuarioADO.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//agragar usion
+using System.Data;
+using System.Data.SqlClient;
+using ProyVentas_ADO;
+using ProyVentas_BE;
+
+namespace ProyVentas_ADO
+{
+    public class UsuarioADO
+    {
+        ConexionADO MiConexion = new ConexionADO();
+        SqlConnection cnx = new SqlConnection();
+        SqlCommand cmd = new SqlCommand();
+        SqlDataReader dtr;
+
+        public UsuarioBE ConsultarUsuario(String strLogin)
+        {
+            UsuarioBE objUsuarioBE = new UsuarioBE();
+            cnx.ConnectionString = MiConexion.GetCnx();
+            cmd.Connection = cnx;
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "usp_ConsultarUsuario";
+
+            try
+            {
+                //agregamos el paramtro
+                cmd.Parameters.Clear();
+                cmd.Parameters.AddWithValue("@Login_Usuario", strLogin);
+                cnx.Open();
+                dtr = cmd.ExecuteReader();
+                if (dtr.HasRows)
+                {
+                    dtr.Read();
+                    objUsuarioBE.LogUsu = dtr["LogUsu"].ToString();
+                    objUsuarioBE.PassUsu = dtr["PassUsu"].ToString();
+                    objUsuarioBE.NivUsu = Convert.ToInt16(dtr["NivUsu"]);
+                    objUsuarioBE.FecReg = Convert.ToDateTime(dtr["FecReg"]);
+                    objUsuarioBE.UsuReg = dtr["UsuReg"].ToString();
+                    objUsuarioBE.EstUsu = Convert.ToInt16(dtr["EstUsu"]);
+                }
+                dtr.Close();
+                return objUsuarioBE;
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                if (cnx.State == ConnectionState.Open)
+                {
+                    cnx.Close();
+                }
+            }
+        }
+    }
+}
---
+++ ProyColegio_BE/UsuarioBE.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyVentas_BE
+{
+    public class UsuarioBE
+    {
+        private String mvarlogin_usuario;
+        private String mvarpass_usuario;
+        private Int16 mvarniv_usuario;
+        private Int16 mvarest_usuario;
+        private DateTime mvarfec_registro;
+        private String mvarusu_registro;
+
+        public String LogUsu
+        {
+            get { return mvarlogin_usuario; }
+            set { mvarlogin_usuario = value; }
+        }
+
+        public String PassUsu
+        {
+            get { return mvarpass_usuario; }
+            set { mvarpass_usuario = value; }
+        }
+
+        public Int16 NivUsu
+        {
+            get { return mvarniv_usuario; }
+            set { mvarniv_usuario = value; }
+        }
+
+        public Int16 EstUsu
+        {
+            get { return mvarest_usuario; }
+            set { mvarest_usuario = value; }
+        }
+
+        public DateTime FecReg
+        {
+            get { return mvarfec_registro; }
+            set { mvarfec_registro = value; }
+        }
+
+        public String UsuReg
+        {
+            get { return mvarusu_registro; }
+            set { mvarusu_registro = value; }
+        }
+
+    }
+}
