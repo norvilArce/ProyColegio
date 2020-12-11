61bfc93b198883ab42ed3139a8640f8b1af73ac9
Jean Pierre Lino <darkjean059@gmail.com>
2020-12-11 15:49:53 -0500

    jean pierre

---
+++ SitioColegioWEB_GUI/WebLogout.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SitioVentasWEB_GUI
+{
+    public partial class WebLogout : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnFinSesion_Click(object sender, EventArgs e)
+        {
+          
+                System.Web.Security.FormsAuthentication.SignOut();
+                Session.Abandon();
+                Response.Redirect("WebLogin.aspx");
+            
+        }
+    }
+}
